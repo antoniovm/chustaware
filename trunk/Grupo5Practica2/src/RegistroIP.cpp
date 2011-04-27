4d4a9b9eb5c524156588a6ed6e62d778bee7e311
miguel1502@gmail.com <miguel1502@gmail.com@9dce9abb-cf92-60d8-2b11-da8132793504>
2011-04-27 12:30:54 +0000

    RegistroIP y RegistroIS implementadas.

--- trunk/Grupo5Practica2/src/RegistroIP.cpp
+++ trunk/Grupo5Practica2/src/RegistroIP.cpp
@@ -7,10 +7,49 @@
 #include "RegistroIP.h"
 
 RegistroIP::RegistroIP() {
-	// TODO Auto-generated constructor stub
+	strcpy(this->clavePrimaria, "");
+	this->posRegistro = -1;
+	this->siguiente = -1;
+}
 
+RegistroIP::RegistroIP(string clavePri, int posicion, int siguiente) {
+	strcpy(this->clavePrimaria, clavePri.data());
+	this->posRegistro = posicion;
+	this->siguiente = siguiente;
 }
 
 RegistroIP::~RegistroIP() {
-	// TODO Auto-generated destructor stub
+}
+
+char* RegistroIP::getClavePrimaria() const
+{
+	char* cadena;
+	cadena = new char[50];
+	strcpy(cadena, clavePrimaria);
+	return cadena;
+}
+
+int RegistroIP::getPosRegistro() const
+{
+    return posRegistro;
+}
+
+int RegistroIP::getSiguiente() const
+{
+    return siguiente;
+}
+
+void RegistroIP::setClavePrimaria(char* clave)
+{
+	strcpy(this->clavePrimaria, clave);
+}
+
+void RegistroIP::setPosRegistro(int posRegistro)
+{
+    this->posRegistro = posRegistro;
+}
+
+void RegistroIP::setSiguiente(int siguiente)
+{
+    this->siguiente = siguiente;
 }
--- trunk/Grupo5Practica2/src/RegistroIS.h
+++ trunk/Grupo5Practica2/src/RegistroIS.h
@@ -8,13 +8,24 @@
 #ifndef REGISTROIS_H_
 #define REGISTROIS_H_
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
 class RegistroIS {
 private:
 	int claveSecundaria;
 	int posPrimero;
 public:
 	RegistroIS();
+	RegistroIS(int, int);
 	virtual ~RegistroIS();
+    int getClaveSecundaria() const;
+    int getPosPrimero() const;
+    void setClaveSecundaria(int claveSecundaria);
+    void setPosPrimero(int posPrimero);
 };
 
 #endif /* REGISTROIS_H_ */
