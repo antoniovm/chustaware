130415fc48438da9b5cea41d0a589ea499ffc0e7
Antonio Vicente Martín <antoniovicente.avm@gmail.com>
2011-05-05 21:39:17 +0000

    Diseño de clases

---
+++ Afinador/src/afinador/src/ConversorTF.java
@@ -0,0 +1,11 @@
+package afinador.src;
+
+public class ConversorTF {
+	//Esta clase convertira los datos del array del dominio del tiempo al dominio de la frecuencia
+	
+	byte [] tiempo;
+	
+	public ConversorTF(byte [] tiempo) {
+		this.tiempo=tiempo;
+	}
+}
