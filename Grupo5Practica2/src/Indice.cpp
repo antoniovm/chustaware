16a355018cbfafaaee93d7a055fd0ba3fef056ef
Miguel Vicente Linares <miviclin@users.noreply.github.com>
2011-05-20 08:21:15 +0000

    mostrar en indice

--- Grupo5Practica2/src/Indice.cpp
+++ Grupo5Practica2/src/Indice.cpp
@@ -60,11 +60,52 @@ int Indice::buscarS(int patas){
 
 }
 
-void Indice::mostrarIP(){
+void Indice::mostrar(string nombre) {
+	fstream archivo(nombre.data(), fstream::in | fstream::binary);
 
-}
-void Indice::mostrarIS(){
+	if (!archivo.is_open()) {
+		cout << "No existe el archivo" << endl;
+		return;
+	}
+	// Comprobamos si el archivo esta vacio
+	archivo.seekg(0, ios::end);
+	if (archivo.tellg() == 0) {
+		cout << "Archivo vacio" << endl;
+		archivo.close();
+		return;
+	}
+	if (nombre == "IP.dat") {
+		RegistroIP registro;
+		cout << "Clave primaria\t|\tPosicion en el archivo de datos\n";
+		while (1) {
+			archivo.read((char*) &registro, sizeof(registro));
+			if (archivo.eof())
+				break;
+			cout << registro.getClavePrimaria() << "\t|\t" << registro.getPosRegistro() << endl;
+		}
+	}
+	if (nombre == "IS.dat") {
+		RegistroIS registro;
+		cout << "Clave secundaria\t|\tPosicion del primero en el archivo auxiliar\n";
+		while (1) {
+			archivo.read((char*) &registro, sizeof(registro));
+			if (archivo.eof())
+				break;
+			cout <<registro.getClaveSecundaria() << "\t|\t"	<< registro.getPosPrimero() << endl;
+		}
+	}
+	if (nombre == "IAux.dat") {
+		RegistroAux registro;
+		cout << "Bit de validez\t|\tClave primaria\t|\tPosicion del siguiente\t|\tPosicion en el archivo de datos\n";
+		while (1) {
+			archivo.read((char*) &registro, sizeof(registro));
+			if (archivo.eof())
+				break;
+			cout << registro.getValido() << "\t|\t" << registro.getClavePrimaria() << "\t|\t" << registro.getSiguiente() << "\t|\t" << registro.getPosDatos() << endl;
+		}
+	}
 
+	archivo.close();
 }
 
 void Indice::crearFicheroBloques(){
